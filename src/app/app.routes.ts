a925c1d98a69e99e54b1d48d6e4d15173fc293a7
Chanuka Lankanjana <chanukalankanjana1119@gmail.com>
2025-05-19 03:46:23 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
+import { BannerComponent } from './pages/banner/banner.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: BannerComponent
+    },
     {
         path: 'booking',
         loadComponent: () => import('./pages/booking/booking.component').then(m => m.BookingComponent)  
--- src/app/pages/banner/banner.component.ts
+++ src/app/pages/banner/banner.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-banner',
-  imports: [],
+  selector: 'app-event-booking',
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.css'
+  styleUrls: ['./banner.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule]
 })
 export class BannerComponent {
+  event = {
+    title: 'Wenas Nights',
+    subtitle: 'BNS, Raini, Sarith Surith with News',
+    date: '2025 / 10 /06',
+    venue: 'Saffron Beach hotel, Wadduwa',
+    ticketPrices: [
+      { type: 'SILVER', price: 3500 },
+      { type: 'GOLD', price: 5000 },
+      { type: 'PLATINUM', price: 7500 }
+    ],
+    bannerImage:"banner.jpeg",
+  };
 
+  constructor(private router: Router) {}
+
+  navigateToBooking() {
+    this.router.navigate(['/booking']);
+  }
 }
