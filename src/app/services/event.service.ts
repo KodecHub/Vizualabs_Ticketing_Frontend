35c0a28525f17d81ee24b2a199bb55fa0d0c100d
M.D.W.De Silva <wdulanjith@gmail.com>
2025-06-01 18:49:53 +0530

    event registration api configured

---
+++ src/app/services/event.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventService {
+  private baseUrl: string = 'http://localhost:8080/api';
+
+  constructor(private http: HttpClient) {}
+
+  registerEvent(
+    eventName: string,
+    eventId: string,
+    requestBody: any
+  ): Observable<any> {
+    const params = new HttpParams()
+      .set('eventName', eventName)
+      .set('eventId', eventId);
+
+    return this.http.post<any>(`${this.baseUrl}/register-event`, requestBody, {
+      params,
+    });
+  }
+}
